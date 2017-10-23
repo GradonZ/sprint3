34bbdc7e9b9d449b49267e9bbdc2a6b8214ee1bc
tiaartjeee <32131377+tiaartjeee@users.noreply.github.com>
2017-10-23 10:01:58 +0200

    Add files via upload

---
+++ score_plus_colors.ino
@@ -0,0 +1,62 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_NeoPixel.h>
+#define PIN 7
+LiquidCrystal_I2C lcd(0x3F,16,2); 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
+
+int fsrAnalogPin = 0; // FSR is connected to analog 0
+int fsrReading;      // The analog reading from the pin that the FSR is connected to
+int counterState = 0;
+int prevCounterState = 0;
+long counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+  lcd.init(); //initialises the LCD screen so i can actually use it later
+  strip.begin();
+  strip.setBrightness(80);
+}
+
+void loop() {
+  fsrReading = analogRead(fsrAnalogPin);
+
+  //Serial.println(fsrReading);
+  //delay(100);
+  counterState = fsrReading;
+  if (counterState < 100 && prevCounterState > 100)
+  {
+    counter = counter +1;
+    Serial.println(counter);
+  }
+  prevCounterState = counterState;
+   //lcd.clear(); //clears all remaining numbers/ letter on screen because otherwise they'll stay there
+   lcd.backlight(); //enables the LCD screen's backlight
+   lcd.setCursor(3,0); //3 spaces in on the first row
+   lcd.print("Score:");
+   lcd.setCursor(3,3); //2 spaces in on the third row
+   lcd.print(counter);
+   
+   pixelring();
+}
+
+void pixelring(){
+  if(counter<3){                           //color level 1
+    for(int x=0; x<12; x++){
+      strip.setPixelColor(x, 0, 210, 20);   //set color green
+    }
+  }
+  else if(3<=counter<6){                     //color level 2
+    for(int x=0; x<12; x++){
+      strip.setPixelColor(x, 255, 210, 0);    //set color yellow
+    }
+  }
+  else if(6<=counter){                        //color level 3
+    for(int x=0; x<12; x++){
+      strip.setPixelColor(x, 255, 75, 0);      //set color orange
+    }
+  }
+
+
+}
+
---
+++ servomotoren_simon.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+Servo myservo;            // create servo object to control a servo
+Servo myservo2; 
+Servo myservo3;                     
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(8);  // attaches the servo on a pin to the servo object
+  myservo2.attach(7);
+  myservo3.attach(6);
+}
+
+void loop() {
+  
+  for (pos = 0; pos <= 150; pos += 1) { // goes from 0 degrees to 150 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo2.write(pos);
+    myservo3.write(pos);
+    delay(20);                       // waits 20ms for the servo to reach the position
+  }
+  for (pos = 150; pos >= 0; pos -= 1) { // goes from 150 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    myservo2.write(pos);
+    myservo3.write(pos);
+    delay(20);                       // waits 20ms for the servo to reach the position
+  }
+  
+}
+
+
+
